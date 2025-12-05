cc842b803d2dd3b6918f9fa5c1f54eecd64fb16e
SzymonPT <polytopia72@gmail.com>
2025-12-05 12:12:41 +0100

    połączenie z internetem

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -8,12 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://my-json-server.typicode.com/SzymonPT/retrofitL1/");
+        .addConverterFactory(GsonConverterFactory.create())
+            .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Pytanie>> call = jsonPlaceHolderApi.getPytania();
+        call.enqueue( );
     }
 }
